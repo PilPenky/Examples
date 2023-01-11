c75f79a0a3350954fb124425d4a8599d76b2c244
Artur Pilipenko <apg-a@mail.ru>
2023-01-11 17:32:26 +0200

    Таблица умножения - цикл в цикле

---
+++ Lesson3/Example3-04-CycleFor/Program.cs
@@ -0,0 +1,12 @@
+﻿// 4 вид метода в цикле for:Принимает и возвращает:
+string Method4(int count, string text)
+{
+    string result = String.Empty; //String.Empty - это пуская строка, тоже самое, что указать: string result = ""
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "Какой-то текст\n");
+Console.WriteLine(res); 
