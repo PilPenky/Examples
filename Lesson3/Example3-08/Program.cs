cd6c38532250a4dc2752cf79eaa02b3ed00eba0e
Artur Pilipenko <apg-a@mail.ru>
2023-01-12 17:32:31 +0200

    Формирование по порядку массива чисел от большего к меньшему

---
+++ Lesson3/Example3-08/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача:
+Массив чисел 1, 5, 4, 3, 2, 6, 7, 1, 1 - сформировать от большего к меньшему */
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+// Метод вывода массива:
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// Метод сформирования чисел по порядку:
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary =  array[maxPosition];
+        array[maxPosition] = array[i];
+        array[i] = temporary;
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
