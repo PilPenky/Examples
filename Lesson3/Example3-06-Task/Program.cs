da32cb0d9f032e063fbfa93b08df6312dde43fc8
Artur Pilipenko <apg-a@mail.ru>
2023-01-12 15:51:19 +0200

    Четвертый метод в цикле for

---
+++ Lesson3/Example3-06-Task/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача:
+Дан текст. В тексте нужно все пробелы заменить черточками,
+маленькие буквы “к” заменить большими “К”, а большие “В” заменить
+маленькими “в”. */
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+/* string s = "q w e r t y"
+               0 1 2 3 5 6
+s[3] будет равняться r
+s[y] будет равняться y */
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty; //String.Empty - пустая строка
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'В', 'в');
+Console.WriteLine(newText);
