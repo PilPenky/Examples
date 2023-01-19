f6f75beceeae2ca78aee082e25d09302354b0c38
Artur Pilipenko <apg-a@mail.ru>
2023-01-19 17:07:35 +0200

    Пример рекурсии в факториале

---
+++ Lesson4/Example4-04/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пример рекурсии в факториале:
+
+double Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n-1); //метод вызывает сам себя
+}
+
+for(int i = 0; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+//Console.WriteLine(Factorial(3)); // 3*2*1 = 6
