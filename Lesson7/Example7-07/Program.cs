0ced6dcc494aa05f5870cf4d280fc183fd5ef018
Artur Pilipenko <apg-a@mail.ru>
2023-01-29 18:16:19 +0200

    Рекурсия. Решение игры Пирамидки

---
+++ Lesson7/Example7-07/Program.cs
@@ -0,0 +1,10 @@
+﻿// Игра "Пирамидки" - https://yandex.ru/games/app/193642
+
+//Решение игры:
+void Towers(string with = "1", string on = "3", string some = "2", int count = 5)
+{
+if (count > 1) Towers(with, some, on, count - 1);
+Console.WriteLine($"{with} >> {on}");
+if (count > 1) Towers(some, on, with, count - 1);
+}
+Towers();
