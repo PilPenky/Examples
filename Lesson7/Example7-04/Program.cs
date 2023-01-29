58bdcf0bad361065c911e47b6ed79ed32a75c73f
Artur Pilipenko <apg-a@mail.ru>
2023-01-29 18:13:30 +0200

    Рекурсия. Задача: Вычислить A в степени N

---
+++ Lesson7/Example7-04/Program.cs
@@ -0,0 +1,19 @@
+﻿// Вычислить A в степени N
+
+// Решение задачи без рекурсии:
+int PowerFor(int a, int n)
+{ 
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= a;
+    return result;
+}
+Console.WriteLine(PowerFor(2, 10)); // 1024
+
+// Решение задачи c рекурсией:
+int PowerRec(int a, int n)
+{ 
+    //return n == 0 ? 1 : PowerRec(a, n - 1) * a; //Сокращение нижеописанного кода
+    if (n == 0) return 1; //Условие выхода из рекурсии
+    else return PowerRec(a, n - 1) * a;
+}
+Console.WriteLine(PowerRec(2, 10)); // 1024
