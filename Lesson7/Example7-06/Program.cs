9f4eb307a0b3610ccc32ff1c077922110073a5e6
Artur Pilipenko <apg-a@mail.ru>
2023-01-29 18:15:29 +0200

    Рекурсия. Как посмотреть содержимое папки в ПК

---
+++ Lesson7/Example7-06/Program.cs
@@ -0,0 +1,17 @@
+﻿// Как посмотреть содержимое папки:
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+    foreach (var currentCatalog in catalogs.GetDirectories())
+    {
+        Console.WriteLine($"{indent}{currentCatalog.Name}");
+        CatalogInfo(currentCatalog.FullName, indent + " ");
+    }
+    foreach (var item in catalogs.GetFiles())
+    {
+        Console.WriteLine($"{indent}{item.Name}");
+    }
+}
+string path = @"C:\Users\Admin\Desktop\Учёба\!Разработчик!\3. Знакомство с языками программирования (лекции) - Сергей Камянецкий\Examples\Lesson1\Example001";
+CatalogInfo(path);
